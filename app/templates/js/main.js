bd55ba3329e91c5f4c269f30a6e77648d6180b9e
stanislavorlov <stasorlov21@gmail.com>
2026-08-29 19:50:22 +0300

    added zoom control and moved it to the suitable place

--- app/templates/js/main.js
+++ app/templates/js/main.js
@@ -1,8 +1,11 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map', { zoomControl: false }).setView([51.505, -0.09], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// Default zoom control sits top-left, which collides with the search panel.
+L.control.zoom({ position: 'topright' }).addTo(map);
 
 var popup = L.popup();
 
