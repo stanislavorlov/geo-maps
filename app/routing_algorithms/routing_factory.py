cb3481e47d0c9c2370e744ca2b69162b6c7cbf56
stanislavorlov <stasorlov21@gmail.com>
2026-09-14 20:49:55 +0300

    design patter for routing factory

--- app/routing_algorithms/routing_factory.py
+++ app/routing_algorithms/routing_factory.py
@@ -5,31 +5,56 @@ from routing_algorithms.dijkstra_routing import DijkstraRoutingAlgorithm
 from routing_algorithms.astar_routing import AStarRoutingAlgorithm
 from services.speed_limit_service import SpeedLimitService
 from services.travel_time_service import TravelTimeService
+from typing import Type
 
+RoutingAlgorithmKey = tuple[str, bool]
+RoutingAlgorithmClass = Type[AbstractRoutingAlgorithm]
+
+class RoutingAlgorithmFactory:
+
+    _algorithms: dict[
+        RoutingAlgorithmKey,
+        RoutingAlgorithmClass
+    ] = {
+        ("dijkstra", False): DijkstraRoutingAlgorithm,
+        ("dijkstra", True): BidirectionalDijkstraRoutingAlgorithm,
+        ("astar", False): AStarRoutingAlgorithm,
+        # ("astar", True): BidirectionalAStarRoutingAlgorithm,
+        # ("bellman_ford", False): BellmanFordRoutingAlgorithm,
+    }
+
+    @classmethod
+    def create(
+        cls,
+        request: RouteRequest,
+        speed_limit_service: SpeedLimitService,
+        travel_time_service: TravelTimeService,
+    ) -> AbstractRoutingAlgorithm:
+
+        key = (request.routeType, request.bidirectional)
+
+        algorithm_class = cls._algorithms.get(key)
+
+        if algorithm_class is None:
+            raise NotImplementedError(
+                f"Routing algorithm '{request.routeType}' "
+                f"with bidirectional={request.bidirectional} "
+                f"is not supported"
+            )
+
+        return algorithm_class(
+            speed_limit_service=speed_limit_service,
+            travel_time_service=travel_time_service,
+        )
 
 def get_routing_algorithm(
     request: RouteRequest,
     speed_limit_service: SpeedLimitService,
     travel_time_service: TravelTimeService
 ) -> AbstractRoutingAlgorithm:
-    match request.routeType:
-        case "dijkstra":
-            if request.bidirectional:
-                return BidirectionalDijkstraRoutingAlgorithm(
-                    speed_limit_service=speed_limit_service,
-                    travel_time_service=travel_time_service,
-                )
-            return DijkstraRoutingAlgorithm(
-                speed_limit_service=speed_limit_service,
-                travel_time_service=travel_time_service
-            )
-        case "astar":
-            # ToDo:
-            if request.bidirectional:
-                pass
-            return AStarRoutingAlgorithm(
-                speed_limit_service=speed_limit_service,
-                travel_time_service=travel_time_service
-            )
-        case _:
-            raise NotImplementedError(f"Route type '{request.routeType}' is not supported")
+
+    return RoutingAlgorithmFactory.create(
+        request=request,
+        speed_limit_service=speed_limit_service,
+        travel_time_service=travel_time_service,
+    )
